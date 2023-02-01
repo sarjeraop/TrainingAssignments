eec42992aa1e1c3f3ccba8a68bdcd236c73162f7
Sarjerao5005801 <sarjerao.p@deciphercloud.com>
2023-02-01 17:48:51 +0530

    Added comments

--- force-app/main/default/aura/AuraCalculator/AuraCalculatorController.js
+++ force-app/main/default/aura/AuraCalculator/AuraCalculatorController.js
@@ -1,4 +1,11 @@
 ({
+
+/* * @methodname         press
+   * @author             Sarjerao 
+   * @group              none
+   * @param              none
+   * @description        This Press method is used for get Button value From button And Pass To The Veriable while fireing event .
+   * @return             none */
     press : function(component, event, helper) {
         var buttonName = event.getSource().get("v.name");
         console.log('buttonName=='+buttonName);
