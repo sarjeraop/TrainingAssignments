dcdb85e781f871dc9a3060110f5a6e2abcb17218
Sarjerao5005801 <sarjerao.p@deciphercloud.com>
2023-02-02 17:16:43 +0530

    Comments Added Controller Of Button cmp

--- force-app/main/default/aura/AuraCalculator/AuraCalculatorController.js
+++ force-app/main/default/aura/AuraCalculator/AuraCalculatorController.js
@@ -1,12 +1,19 @@
 ({
-    press : function(component, event, helper) {
-        var buttonName = event.getSource().get("v.name");
-        console.log('buttonName=='+buttonName);
-        // Get the component event by using the
-        // name value from aura:registerEvent
-        var cmpEvent = component.getEvent("DisplayVaueEvent");
-        cmpEvent.setParams({"message" : buttonName });
-        cmpEvent.fire();
-        console.log('in press button event');   
-    } 
-})
+
+    /* * @methodname         press
+       * @author             Sarjerao 
+       * @group              none
+       * @param              none
+       * @description        This Press method is used for get Button value From button And Pass To The Veriable while fireing event .
+       * @return             none */
+        press : function(component, event, helper) {
+            var buttonName = event.getSource().get("v.name");
+            console.log('buttonName=='+buttonName);
+            // Get the component event by using the
+            // name value from aura:registerEvent
+            var cmpEvent = component.getEvent("DisplayVaueEvent");
+            cmpEvent.setParams({"message" : buttonName });
+            cmpEvent.fire();
+            console.log('in press button event');   
+        } 
+    })
