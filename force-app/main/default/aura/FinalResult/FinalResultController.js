09465d87c6c45b4caa97d4ea0c12afd0a0d897fe
Sarjerao5005801 <sarjerao.p@deciphercloud.com>
2023-02-02 17:26:43 +0530

    Comments Added in Controller of Finalresult cmp

--- force-app/main/default/aura/FinalResult/FinalResultController.js
+++ force-app/main/default/aura/FinalResult/FinalResultController.js
@@ -1,5 +1,12 @@
 ({
-	showFinalResult : function(cmp, event, helper) {
+	/**	@methodname	:	showFinalResult
+      *	@author	:	Sarjerao
+      *	@group	:	none
+      *	@param	:	none
+      *	@description	:	This Is controller of finalResult Aura cmp to Take final result value and pass to the other aura cmp
+      *	@return	:	
+    */
+    showFinalResult : function(cmp, event, helper) {
          console.log('Result$$');
 		var Result = event.getParam("FinalResult");
         console.log('Result$$'+Result);
