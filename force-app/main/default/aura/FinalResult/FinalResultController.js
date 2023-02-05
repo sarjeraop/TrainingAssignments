9bce844e8f6464a27f31b1ea8a377a0d3e8385ee
Sarjerao5005801 <sarjerao.p@deciphercloud.com>
2023-02-05 15:03:22 +0530

    Changes in Code Indentation,Remove Empty Lines,Remove Debug logs

--- force-app/main/default/aura/FinalResult/FinalResultController.js
+++ force-app/main/default/aura/FinalResult/FinalResultController.js
@@ -1,17 +1,14 @@
 ({
-	/**	@methodname	:	showFinalResult
-      *	@author	:	Sarjerao
-      *	@group	:	none
-      *	@param	:	none
-      *	@description	:	This Is controller of finalResult Aura cmp to Take final result value and pass to the other aura cmp
-      *	@return	:	
-    */
-    showFinalResult : function(cmp, event, helper) {
-         console.log('Result$$');
-		var Result = event.getParam("FinalResult");
-        console.log('Result$$'+Result);
-        cmp.set("v.FResult",Result); 
-        
-       // cmp.set("v.messageFromEvent", message);
+/**	@methodname	           :	showFinalResult
+  *	@author	               :	Sarjerao Pujari
+  *	@ObjectReferenced	     :	none
+  *	@param	               :	none
+  *	@description	         :	This Is controller of finalResult Aura cmp to Take final result value and pass to the other aura cmp
+  *	@return	               :	Void
+  */
+  showFinalResult : function(component, event, helper) 
+  {
+    var Result = event.getParam("FinalResult");
+    component.set("v.FResult",Result);
 	}
 })
