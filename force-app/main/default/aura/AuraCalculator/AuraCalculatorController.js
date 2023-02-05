bb564979842d8972a67f2df36826afe7db7bdede
Sarjerao5005801 <sarjerao.p@deciphercloud.com>
2023-02-05 14:57:53 +0530

    Changes in Code Indentation,Remove Empty Lines,Remove Debug logs

--- force-app/main/default/aura/AuraCalculator/AuraCalculatorController.js
+++ force-app/main/default/aura/AuraCalculator/AuraCalculatorController.js
@@ -1,19 +1,17 @@
 ({
-
-    /* * @methodname         press
-       * @author             Sarjerao 
-       * @group              none
-       * @param              none
-       * @description        This Press method is used for get Button value From button And Pass To The Veriable while fireing event .
-       * @return             none */
-        press : function(component, event, helper) {
-            var buttonName = event.getSource().get("v.name");
-            console.log('buttonName=='+buttonName);
-            // Get the component event by using the
-            // name value from aura:registerEvent
-            var cmpEvent = component.getEvent("DisplayVaueEvent");
-            cmpEvent.setParams({"message" : buttonName });
-            cmpEvent.fire();
-            console.log('in press button event');   
-        } 
-    })
+/* 
+ * @methodname                : Buttonpress
+ * @author                    : Sarjerao Pujari
+ * @ObjectReferenced          : none
+ * @param                     : component, event, helper
+ * @description               : This Press method is used for get Button value From button And Pass To The Veriable while fireing event .
+ * @return                    : none 
+ */
+   Buttonpress : function(component, event, helper) 
+   {
+        var buttonName = event.getSource().get("v.name");
+        var cmpEvent = component.getEvent("DisplayVaueEvent");
+        cmpEvent.setParams({"message" : buttonName });
+        cmpEvent.fire();
+    } 
+})
